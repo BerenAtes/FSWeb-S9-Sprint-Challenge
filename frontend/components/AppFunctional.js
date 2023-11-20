61d30e9d35b1c190ef1f632451204cb687d464d4
Beren Ates <berenatess9918@gmail.com>
2023-11-20 18:40:17 +0300

    sprint 9-challenge

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,78 +1,150 @@
-import React from 'react'
+import React, { useState } from "react";
+import axios from "axios";
 
-// önerilen başlangıç stateleri
-const initialMessage = ''
-const initialEmail = ''
-const initialSteps = 0
-const initialIndex = 4 //  "B" nin bulunduğu indexi
+const initialMessage = "";
+const initialEmail = "";
+const initialSteps = 0;
+const initialIndex = 4; //  "B" nin bulunduğu indexi
 
 export default function AppFunctional(props) {
-  // AŞAĞIDAKİ HELPERLAR SADECE ÖNERİDİR.
-  // Bunları silip kendi mantığınızla sıfırdan geliştirebilirsiniz.
+  const [idxB, setIdxB] = useState(initialIndex);
+  const [cntMove, setCntMove] = useState(initialSteps);
+  const [email, setEmail] = useState(initialEmail);
+  const [message, setMessage] = useState(initialMessage);
 
-  function getXY() {
-    // Koordinatları izlemek için bir state e sahip olmak gerekli değildir.
-    // Bunları hesaplayabilmek için "B" nin hangi indexte olduğunu bilmek yeterlidir.
+  function getXY(idx) {
+    const x = (idx % 3) + 1;
+
+    const y = Math.floor(idx / 3) + 1;
+    return [x, y];
   }
 
-  function getXYMesaj() {
-    // Kullanıcı için "Koordinatlar (2, 2)" mesajını izlemek için bir state'in olması gerekli değildir.
-    // Koordinatları almak için yukarıdaki "getXY" helperını ve ardından "getXYMesaj"ı kullanabilirsiniz.
-    // tamamen oluşturulmuş stringi döndürür.
+  function getXYMesaj(coordinates) {
+    return `Koordinatlar (${coordinates[0]}, ${coordinates[1]})`;
   }
 
   function reset() {
-    // Tüm stateleri başlangıç ​​değerlerine sıfırlamak için bu helperı kullanın.
+    setIdxB(initialIndex);
+    setCntMove(initialSteps);
+    setEmail(initialEmail);
+    setMessage(initialMessage);
   }
 
-  function sonrakiIndex(yon) {
-    // Bu helper bir yön ("sol", "yukarı", vb.) alır ve "B" nin bir sonraki indeksinin ne olduğunu hesaplar.
-    // Gridin kenarına ulaşıldığında başka gidecek yer olmadığı için,
-    // şu anki indeksi değiştirmemeli.
+  function move(direction) {
+    const [x, y] = getXY(idxB);
+    let didMove = false;
+
+    switch (direction) {
+      case "left":
+        if (1 !== x) {
+          setIdxB((idx) => idx - 1);
+          didMove = true;
+        } else {
+          setMessage("Sola gidemezsiniz");
+        }
+
+        break;
+      case "right":
+        if (3 !== x) {
+          setIdxB((idx) => idx + 1);
+          didMove = true;
+        } else {
+          setMessage("Sağa gidemezsiniz");
+        }
+        break;
+      case "up":
+        if (1 !== y) {
+          setIdxB((idx) => idx - 3);
+          didMove = true;
+        } else {
+          setMessage("Yukarıya gidemezsiniz");
+        }
+
+        break;
+      case "down":
+        if (3 !== y) {
+          setIdxB((idx) => idx + 3);
+          didMove = true;
+        } else {
+          setMessage("Aşağıya gidemezsiniz");
+        }
+
+        break;
+    }
+
+    if (didMove) setCntMove((cnt) => cnt + 1);
   }
 
-  function ilerle(evt) {
-    // Bu event handler, "B" için yeni bir dizin elde etmek üzere yukarıdaki yardımcıyı kullanabilir,
-    // ve buna göre state i değiştirir.
+  function onChange(event) {
+    setEmail(event.target.value);
   }
 
-  function onChange(evt) {
-    // inputun değerini güncellemek için bunu kullanabilirsiniz
-  }
+  function onSubmit(event) {
+    event.preventDefault();
+    const [x, y] = getXY(idxB);
 
-  function onSubmit(evt) {
-    // payloadu POST etmek için bir submit handlera da ihtiyacınız var.
+    const data = {};
+
+    data.x = x;
+    data.y = y;
+    data.steps = cntMove;
+    data.email = email;
+
+    setEmail("");
+
+    axios
+      .post("http://localhost:9000/api/result", data)
+      .then(function (response) {
+        setMessage(response.data.message);
+      })
+      .catch(function (error) {
+        setMessage(error.response.data.message);
+      });
   }
 
   return (
     <div id="wrapper" className={props.className}>
       <div className="info">
-        <h3 id="coordinates">Koordinatlar (2, 2)</h3>
-        <h3 id="steps">0 kere ilerlediniz</h3>
+        <h3 id="coordinates">{getXYMesaj(getXY(idxB))}</h3>
+        <h3 id="steps">{cntMove} kere ilerlediniz</h3>
       </div>
       <div id="grid">
-        {
-          [0, 1, 2, 3, 4, 5, 6, 7, 8].map(idx => (
-            <div key={idx} className={`square${idx === 4 ? ' active' : ''}`}>
-              {idx === 4 ? 'B' : null}
-            </div>
-          ))
-        }
+        {[0, 1, 2, 3, 4, 5, 6, 7, 8].map((idx) => (
+          <div key={idx} className={`square${idx === idxB ? " active" : ""}`}>
+            {idx === idxB ? "B" : null}
+          </div>
+        ))}
       </div>
       <div className="info">
-        <h3 id="message"></h3>
+        <h3 id="message">{message}</h3>
       </div>
       <div id="keypad">
-        <button id="left">SOL</button>
-        <button id="up">YUKARI</button>
-        <button id="right">SAĞ</button>
-        <button id="down">AŞAĞI</button>
-        <button id="reset">reset</button>
+        <button onClick={() => move("left")} id="left">
+          SOL
+        </button>
+        <button onClick={() => move("up")} id="up">
+          YUKARI
+        </button>
+        <button onClick={() => move("right")} id="right">
+          SAĞ
+        </button>
+        <button onClick={() => move("down")} id="down">
+          AŞAĞI
+        </button>
+        <button onClick={() => reset()} id="reset">
+          reset
+        </button>
       </div>
-      <form>
-        <input id="email" type="email" placeholder="email girin"></input>
+      <form onSubmit={onSubmit}>
+        <input
+          id="email"
+          type="email"
+          placeholder="email girin"
+          onChange={onChange}
+          value={email}
+        />
         <input id="submit" type="submit"></input>
       </form>
     </div>
-  )
+  );
 }
